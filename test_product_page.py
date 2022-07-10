dfb650b764e1324836ad9ac019023c83ea4acd4d
Vika Denisenko <r.denysenko@flex-solution.com>
2022-07-10 18:16:46 +0300

    add pytest.ini

--- test_product_page.py
+++ test_product_page.py
@@ -14,6 +14,7 @@ from .pages.product_page import ProductPage
                              marks=pytest.mark.xfail)])'''
 
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     product_page = ProductPage(browser, link)
@@ -58,6 +59,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -67,6 +69,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -86,7 +89,7 @@ def test_guest_basket_is_not_empty_after_adding_product(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_empty_basket()
 
-@pytest.mark.login_user
+
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -104,6 +107,7 @@ class TestUserAddToBasketFromProductPage:
         product_page.open()
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         product_page = ProductPage(browser, link)
@@ -114,5 +118,7 @@ class TestUserAddToBasketFromProductPage:
         product_page.get_alert_message_name()
         product_page.get_alert_message_price()
 
+# commands for starting
 # pytest -v --tb=line --language=en test_product_page.py
-#pytest -m login_user
+# pytest -m login_user
+# pytest -s -v -m need_review test_product_page.py
